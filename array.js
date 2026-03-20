74545f9f1801cb65109cdb8a9a86bcf8db14678f
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-03-19 22:29:34 -0400

    Initial logic

---
+++ array.js
@@ -0,0 +1,22 @@
+//Task 1 Array Manipulation Basics
+shoppingList=[];
+let item = document.getelementByID("item");
+let btnItem = document.getelementByID("btnItem")
+
+function addItem(item){
+item = shoppingList.push();
+}
+
+function removeLastItem(){
+    shoppingList.pop();
+}
+
+function displayList(){
+    List.shoppingList;
+}
+
+//Task 2 Filter and Search an Array
+
+function filterItems (items){
+lists the case insensitve items
+}
