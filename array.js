3c9d3938b8fc79e3ab915905defef0125292eec2
Vidhya Divakar <vidhyaminuteread@gmail.com>
2026-03-19 23:51:29 -0400

    added few more logic to the shopping list

--- array.js
+++ array.js
@@ -1,22 +1,44 @@
 //Task 1 Array Manipulation Basics
-shoppingList=[];
+let shoppingList=[];
 let item = document.getelementByID("item");
-let btnItem = document.getelementByID("btnItem")
+let btnItem = document.getelementByID("btnItem");
+let btnRmvItem = document.getelementByID("btnRmvItem");
+let shopList = document.getelementByID("shoppingList");
+let searchItem = document.getelementByID("searchItem");
 
-function addItem(item){
-item = shoppingList.push();
-}
+btnItem.addEventListener("click", function addItem(item){
+    if (item === "") {
+      alert("Please enter an item.");
+      return;
+    }
+     shoppingList.push(item);
+displayList();
+item.value = ""; //clear the input field
+});
 
-function removeLastItem(){
-    shoppingList.pop();
-}
+btnRmvItem.addEventListener("click", function removeLastItem() {
+     shoppingList.pop(); // Remove last item from cart array
+    displayList();
+});
+
 
 function displayList(){
-    List.shoppingList;
+    shopList.innerHTML = "";
+    for (let i=0; i<shoppingList.length; i++){
+        let listItem = document.createElement("li");
+         listItem.innerText = cart[i];
+         shopList.appendChild(listItem);
 }
-
+}
 //Task 2 Filter and Search an Array
 
 function filterItems (items){
-lists the case insensitve items
+return displayList();
 }
+searchItem.addEventListener("click", function filterItems() {
+      // Remove last item from cart array
+    return displayList();
+});
+
+
+    
